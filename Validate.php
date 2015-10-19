a932672ff8377f867c5807a2d88f4aa695fa5fb9
kek91 <kek@teknix.no>
2015-10-19 13:55:32 +0200

    Whitelist validation method

--- Validate.php
+++ Validate.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * PHP Input Validation
+ * Version: 0.2.0
+ * Source:  https://github.com/kek91/PHP-Input-Validation
+ *
+ */
+
 class Validate
 {
     private $_passed = false,
@@ -20,37 +28,37 @@ class Validate
                         break;
                     case 'length_min':
                         if(strlen($value) < $rule_value) {
-                            $this->add_error("<b>{$item}</b> must contain minimum <b>{$rule_value}</b> characters.");
+                            $this->add_error("<b>{$item}</b> must contain minimum <b>{$rule_value}</b> characters");
                         }
                         break;
                     case 'length_max':
                         if(strlen($value) > $rule_value) {
-                            $this->add_error("<b>{$item}</b> can't contain more than <b>{$rule_value}</b> characters.");
+                            $this->add_error("<b>{$item}</b> can't contain more than <b>{$rule_value}</b> characters");
                         }
                         break;
                     case 'matches':
-                        if($value != htmlspecialchars(trim($source[$rule_value]), ENT_QUOETS, 'UTF-8')) {
-                            $this->add_error("<b>{$rule_value}</b> must match <b>{$item}</b>.");
+                        if($value != htmlspecialchars(trim($source[$rule_value]), ENT_QUOTES, 'UTF-8')) {
+                            $this->add_error("<b>{$item}</b> must match <b>{$rule_value}</b>");
                         }
                         break;
                     case 'mailcheck':
                         if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                            $this->add_error("<b>{$value}</b> is not a valid email address.");
+                            $this->add_error("<b>{$value}</b> is not a valid email address");
                         }
                         break;
                     case 'numeric':
-                        if(!ctype_digit($value)) {
-                            $this->add_error("<b>{$item}</b> contains illegal characters. Only numbers 0-9 are allowed.");
+                        if(!ctype_digit(str_replace('+', '', $value))) {
+                            $this->add_error("<b>{$item}</b> contains illegal characters. Only numbers 0-9 and \"+\"-sign are allowed");
                         }
                         break;
                     case 'alphabetic':
                         if (!ctype_alpha(str_replace(array(' ', "'", '-'), '', $value))) {
-                            $this->add_error("<b>{$item}</b> contains illegal characters. Only alphabetic letters A-Z, \"'\", \" \" and \"-\" are allowed.");
+                            $this->add_error("<b>{$item}</b> contains illegal characters. Only alphabetic letters A-Z, \"'\", \" \" and \"-\" are allowed");
                         }
                         break;
                     case 'alphanumeric':
                         if(!ctype_alnum($value)) {
-                            $this->add_error("<b>{$item}</b> contains illegal characters. Only alphabetic and numeric characters (A-Z and 0-9) are allowed.");
+                            $this->add_error("<b>{$item}</b> contains illegal characters. Only alphabetic and numeric characters (A-Z and 0-9) are allowed");
                         }
                         break;
                     case 'blacklist':
@@ -60,6 +68,16 @@ class Validate
                             }
                         }
                         break;
+                    case 'whitelist':
+                        foreach($rule_value as $approved_word) {
+                            if($value == $approved_word) {
+                                $match = true;
+                                break;
+                            }
+                        }
+                        if(!$match) {
+                            $this->add_error("<b>{$value}</b> is blocked");
+                        }
                 }
             }
         }
